4f67ebd26efc63adf8e129548339b3bc2c9ea3fd
Gabriele Sbaiz <gabriele.sbaiz@2601.it>
2023-04-04 10:11:54 +0200

    Add Cache

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
 use Magarrent\LaravelUrlShortener\Models\UrlShortener;
 
-$shortUrls = UrlShortener::all();
+$shortUrls = Cache::tags(['urlShortener'])
+    ->rememberForever('urlShortenerAll', function () {
+        return UrlShortener::all();
+    });
 
 foreach ($shortUrls as $url) {
     Route::redirect($url->url_key, $url->to_url, 301);
